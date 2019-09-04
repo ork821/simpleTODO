0ef1905373b09939a2a85a2c8826b2fd923b462b
ork821 <ork821@mail.ru>
2019-09-04 09:29:46 +0300

    deleting on click 'delete' button

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ window.addEventListener('load', () => {
     const todo_field = document.getElementById('todo_field');
     const inputElement = document.getElementsByClassName('form-control')[0];
     let inputText = '';
-    const btn = document.getElementsByClassName('.delete');
-
-
 
     const addTask = () => {
         let to_do = document.createElement('li');
@@ -15,11 +12,6 @@ window.addEventListener('load', () => {
         todo_field.append(to_do);
     }
 
-    const deleteTask = () => {
-        console.log(todo_field.children);
-    }
-
-
     inputElement.addEventListener('keydown', (e) => {
         if (e.keyCode === 13) {
             inputText = inputElement.value;
@@ -29,12 +21,11 @@ window.addEventListener('load', () => {
     });
 
     todo_field.addEventListener('click', (e) => {
-       e.target.parentElement.remove();
+       if (e.target.classList.contains('delete')) {
+           e.target.parentElement.remove();
+       }
+
     });
-
-
-
-
 
 
 });
